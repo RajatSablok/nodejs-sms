9273db52764664cfbed19f1583f8ab3242b805f7
RajatSablok <rajat.main06@gmail.com>
2020-07-07 17:11:31 +0530

    added sms with nexmo service

---
+++ nexmo.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const NEXMO_API_KEY = process.env.NEXMO_API_KEY;
+const NEXMO_API_SECRET = process.env.NEXMO_API_SECRET;
+const TO_NUMBER = process.env.NEXMO_TO_NUMBER;
+const NEXMO_BRAND_NAME = process.env.NEXMO_BRAND_NAME;
+
+const Nexmo = require("nexmo");
+
+const nexmo = new Nexmo({
+  apiKey: NEXMO_API_KEY,
+  apiSecret: NEXMO_API_SECRET,
+});
+
+const from = NEXMO_BRAND_NAME;
+const to = TO_NUMBER;
+const text = "A text message sent using the Nexmo SMS API";
+
+nexmo.message.sendSms(from, to, text, (err, responseData) => {
+  if (err) {
+    console.log(err);
+  } else {
+    if (responseData.messages[0]["status"] === "0") {
+      console.log("Message sent successfully.");
+    } else {
+      console.log(
+        `Message failed with error: ${responseData.messages[0]["error-text"]}`
+      );
+    }
+  }
+});
